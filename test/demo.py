ee3e788f823eb0a78a1516e031d4d075e66088d0
Ivo <ivobusko@gmail.com>
2023-02-28 14:03:26 -0500

    experiments with color sensor

--- test/demo.py
+++ test/demo.py
@@ -1,6 +1,8 @@
 import time
 import logging
+import statistics as stat
 from time import sleep
+from colorsys import rgb_to_hsv
 
 from pylgbst.hub import SmartHub, RemoteHandset
 from pylgbst.peripherals import Peripheral, EncodedMotor, TiltSensor, Current, Voltage, COLORS, COLOR_BLACK, COLOR_GREEN
@@ -64,17 +66,70 @@ def demo_motor(hub):
 def demo_color_sensor(smart_hub):
     print("Color sensor test: wave your hand in front of it")
     demo_color_sensor.cnt = 0
-    limit = 100
+    limit = 300
 
-    def callback(color, argument, distance=None):
+    h_list = []
+    s_list = []
+    v_list = []
+
+    def callback(*args, **kwargs):
         demo_color_sensor.cnt += 1
-        print(demo_color_sensor.cnt, limit, COLORS[color], argument, distance)
 
-    smart_hub.vision_sensor.subscribe(callback)
+        # use HSV as criterion for mapping colors
+        r = args[0]
+        g = args[1]
+        b = args[2]
+        h, s, v = rgb_to_hsv(r, g, b)
+        if h > 1:
+            h = 1. - h
+
+        if max(r, g, b) > 10.0 and v > 20.0:
+            h_list.append(h)
+            s_list.append(s)
+            v_list.append(v)
+            print(demo_color_sensor.cnt, limit, args, kwargs, h, s, v)
+
+    smart_hub.vision_sensor.subscribe(callback, granularity=5, mode=6)
+
     while demo_color_sensor.cnt < limit:
         time.sleep(1)
 
     smart_hub.vision_sensor.unsubscribe(callback)
+
+    print("H stats: ", stat.mean(h_list), stat.stdev(h_list), min(h_list), max(h_list))
+    print("S stats: ", stat.mean(s_list), stat.stdev(s_list), min(s_list), max(s_list))
+    print("V stats: ", stat.mean(v_list), stat.stdev(v_list), min(v_list), max(v_list))
+
+    # each row lists mean, stddev, min, max.
+    # red tile
+    # H stats:  0.9831216632280405 0.0052472558028119655 0.9572916666666667 0.9950980392156863
+    # S stats:  0.8529268586460793 0.028608110545456207 0.8152173913043478 0.96875
+    # V stats:  163.19607843137254 31.911570197026187 69.0 278.0
+
+    # white tile
+    # H stats:  0.7637420853168304 0.022818653223407467 0.7083333333333334 0.8333333333333334
+    # S stats:  0.15673942718972475 0.04220747621377944 0.05555555555555555 0.2546583850931677
+    # V stats:  217.53246753246754 23.024747720056183 159.0 283.0
+
+    # dark blue tile (not to be used)
+    # H stats:  0.657232420420557 0.05940983030915233 0.5490196078431372 0.9320987654320988
+    # S stats:  0.4502750567049586 0.10337623196677968 0.09090909090909091 0.6842105263157895
+    # V stats:  34.99261992619926 6.903564629381398 21.0 59.0
+
+    # yellow train body - values spread around origin
+    # H stats:  0.9894320853790294 0.003909771634062216 0.9810126582278481 0.9981684981684982
+    # S stats:  0.6331090625363353 0.013603490642704025 0.6063829787234043 0.6694915254237288
+    # V stats:  165.92409240924093 59.60274822893457 90.0 292.0
+
+    # gray track
+    # H stats:  0.6240816726904218 0.048370112846115546 0.4583333333333333 0.7424242424242425
+    # S stats:  0.23150187264977184 0.04753992183667234 0.08235294117647059 0.38333333333333336
+    # V stats:  68.44408945686901 9.540949457788486 43.0 97.0
+
+    # carpet- values spread around origin
+    # H stats:  0.05236174877835684 0.05367047447782348 0.012820512820512811 0.9523809523809523
+    # S stats:  0.3151364968520766 0.027335502239051256 0.23333333333333334 0.3888888888888889
+    # V stats:  72.24422442244224 24.728052506630195 24.0 170.0
 
 
 DEMO_CHOICES = {
--- test/modes_scanner.py
+++ test/modes_scanner.py
@@ -5,23 +5,24 @@ import time
 from pylgbst.hub import SmartHub, RemoteHandset
 from pylgbst.peripherals import Peripheral, EncodedMotor, TiltSensor, Current, Voltage, COLORS, COLOR_BLACK, COLOR_GREEN
 
-logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
 
-# hub = SmartHub(address='86996732-BF5A-433D-AACE-5611D4C6271D')   # test hub
+hub = SmartHub(address='86996732-BF5A-433D-AACE-5611D4C6271D')   # test hub
 # hub = HandsetRemote(address='2BC6E69B-5F56-4716-AD8C-7B4D5CBC7BF8')  # test handset
-hub = RemoteHandset(address='5D319849-7D59-4EBB-A561-0C37C5EF8DCD')  # train handset
+# hub = RemoteHandset(address='5D319849-7D59-4EBB-A561-0C37C5EF8DCD')  # train handset
 
 descr = {}
 values = hub.peripherals.values()
 
 print("@@@@ modes_scanner.py 15: ", values)
 
-for dev in values:
-    # descr[str(dev)] = dev.describe_possible_modes()
-    # print("@@@@ modes_scanner.py 15: ", dev)
-    print("@@@@ modes_scanner.py 16: ", dev.describe_possible_modes())
+# for dev in values:
+dev = hub.peripherals[1]
+descr[str(dev)] = dev.describe_possible_modes()
+# print("@@@@ modes_scanner.py 15: ", dev)
+print("@@@@ modes_scanner.py 16: ", dev.describe_possible_modes())
 
-# print(descr)
-#
-# with open("descr.json", "w") as fhd:
-#     json.dump(descr, fhd, indent=True)
+print("@@@@ modes_scanner.py 24: ", descr )
+
+with open("descr_color_sensor.json", "w") as fhd:
+    json.dump(descr, fhd, indent=True)
