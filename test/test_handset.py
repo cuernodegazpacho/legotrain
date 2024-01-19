d4019826ab81e9c35fb3b29ca384b678011d9476
cuernodegazpacho <cuernodegazpacho@gmail.com>
2024-01-19 09:38:00 -0500

    test/demo for 2-handset config

--- test/test_handset.py
+++ test/test_handset.py
@@ -7,32 +7,11 @@ from pylgbst.hub import RemoteHandset, RemoteButton, MsgHubAttachedIO
 
 
 remote1 = RemoteHandset(address='CA1ADD7D-6619-B0DF-5D02-99B731959396')  # test handset
-# remote2 = RemoteHandset(address='C8DEE900-B1ED-F26B-7992-6DC06438ADB5')  # train handset
+time.sleep(4)
+remote2 = RemoteHandset(address='C8DEE900-B1ED-F26B-7992-6DC06438ADB5')  # train handset
 
 print(remote1)
-# print(remote2)
-
-# def callback1(value):
-#     print("Voltage granularity=4: %s", value)
-# remote.voltage.subscribe(callback1, mode=Voltage.VOLTAGE_L, granularity=4)
-# time.sleep(10)
-# remote.voltage.unsubscribe(callback1)
-
-# def demo_led_colors(remote):
-#     # LED colors demo
-#     print("LED colors demo")
-#
-#     # We get a response with payload and port, not x and y here...
-#     def colour_callback(named):
-#         print("LED Color callback: %s", named)
-#
-#     remote.led.subscribe(colour_callback)
-#     for color in list(COLORS.keys())[1:] + [COLOR_BLACK, COLOR_GREEN]:
-#         print("Setting LED color to: %s", COLORS[color])
-#         remote.led.set_color(color)
-#         time.sleep(1)
-#
-# demo_led_colors(remote)
+print(remote2)
 
 class HandsetEvent:
     def __init__(self, button):
@@ -73,39 +52,31 @@ class HandsetHandler:
             # difference between the button presses.
             if button_0 is RemoteButton.RED and button_1 is RemoteButton.RED and \
                 abs(timestamp_0 - timestamp_1) < 0.5:
-                print("Dual RED button press")
+                print("Dual RED button press", self.handset)
                 self.buffer = []
 
             # a long button press is indicated by a button press followed by a
             # button release, with a significant time delay between them.
             if button_0 is RemoteButton.RED and button_1 is RemoteButton.RELEASE and \
                 abs(timestamp_0 - timestamp_1) > 2.:
-                print("Long RED button press")
+                print("Long RED button press", self.handset)
                 self.buffer = []
 
             # fall thru
 
 
-remote_handler = HandsetHandler(remote1)
+remote_handler_1 = HandsetHandler(remote1)
+remote_handler_2 = HandsetHandler(remote2)
 
-remote_handler.handset.port_A.subscribe(remote_handler.callback_from_button, mode=2)
-remote_handler.handset.port_B.subscribe(remote_handler.callback_from_button)
+remote_handler_1.handset.port_A.subscribe(remote_handler_1.callback_from_button, mode=2)
+remote_handler_1.handset.port_B.subscribe(remote_handler_1.callback_from_button)
+remote_handler_2.handset.port_A.subscribe(remote_handler_2.callback_from_button, mode=2)
+remote_handler_2.handset.port_B.subscribe(remote_handler_2.callback_from_button)
 
 time.sleep(60)
 
-remote_handler.handset.port_A.unsubscribe(remote_handler.callback_from_button)
-remote_handler.handset.port_B.unsubscribe(remote_handler.callback_from_button)
-
-
-# remote1.port_A.subscribe(callback_from_button_1, mode=2)
-# remote1.port_B.subscribe(callback_from_button_1)
-# time.sleep(60)
-# remote1.port_A.unsubscribe(callback_from_button_1)
-# remote1.port_A.unsubscribe(callback_from_button_1)
-
-# remote2.port_A.subscribe(callback_from_button_2, mode=2)
-# remote2.port_B.subscribe(callback_from_button_2)
-# time.sleep(60)
-# remote2.port_A.unsubscribe(callback_from_button_2)
-# remote2.port_A.unsubscribe(callback_from_button_2)
+remote_handler_1.handset.port_A.unsubscribe(remote_handler_1.callback_from_button)
+remote_handler_1.handset.port_B.unsubscribe(remote_handler_1.callback_from_button)
+remote_handler_2.handset.port_A.unsubscribe(remote_handler_2.callback_from_button)
+remote_handler_2.handset.port_B.unsubscribe(remote_handler_2.callback_from_button)
 
