e828d92fa2b745d5f684b07c31886e32afc3f558
cuernodegazpacho <cuernodegazpacho@gmail.com>
2025-02-22 14:05:56 -0500

    larger range in time@station

--- src/util.py
+++ src/util.py
@@ -2,20 +2,55 @@ import random
 
 import unittest
 
-MINIMUM_TIME_STATION = 2.
-MAXIMUM_TIME_STATION = 12.
+# time ranges for short and long stops
+MINIMUM_TIME_STATION_SHORT = 1.
+MAXIMUM_TIME_STATION_SHORT = 11.
+MINIMUM_TIME_STATION_LONG = 15.
+MAXIMUM_TIME_STATION_LONG = 45.
 
 
-def get_time_station(train):
-    time_station = random.uniform(MINIMUM_TIME_STATION, MAXIMUM_TIME_STATION)
-    return time_station
+class VariableTimerValue:
+    """
+    Class that provides random values for the time a train spends stopped at a station.
+    The values come from two separate uniform distributions, one for short times, the other
+    for longer ones. Successive calls to get_time_station() cyclically return values from
+    the short distribution, then the long.
+    TODO
+    and then zero. The zero value must be handled by
+    the caller; it's initial purpose is to signal the situation in which the train doesn't
+    stop at all.
+    """
+    def __init__(self):
+        self.short = True
+
+    def get_time_station(self):
+        if self.short:
+            time_station = random.uniform(MINIMUM_TIME_STATION_SHORT, MAXIMUM_TIME_STATION_SHORT)
+        else:
+            time_station = random.uniform(MINIMUM_TIME_STATION_LONG, MAXIMUM_TIME_STATION_LONG)
+
+        self.short = not self.short
+
+        return time_station
 
 
 ####################  TEST   ##################################
+
 class TestTimer(unittest.TestCase):
     def test_get_time_station(self):
-        self.assertLessEqual(get_time_station(None), MAXIMUM_TIME_STATION)
-        self.assertGreaterEqual(get_time_station(None), MINIMUM_TIME_STATION)
+
+        # need to construct here to preserve the sequence
+        # with short and long values
+        test_timer = VariableTimerValue()
+
+        t1 = test_timer.get_time_station()
+        t2 = test_timer.get_time_station()
+
+        self.assertLessEqual(t1, MAXIMUM_TIME_STATION_SHORT)
+        self.assertGreaterEqual(t1, MINIMUM_TIME_STATION_SHORT)
+
+        self.assertLessEqual(t2, MAXIMUM_TIME_STATION_LONG)
+        self.assertGreaterEqual(t2, MINIMUM_TIME_STATION_LONG)
 
 if __name__ == '__main__':
     unittest.main()
